942671ca25b348453479ef156181f3f39adc0e2a
srinivasbhuvanagiri <88538870+srinivasbhuvanagiri@users.noreply.github.com>
2021-08-20 17:59:07 +0530

    add code in product-form.js -2.7

--- assets/product-form.js
+++ assets/product-form.js
@@ -9,6 +9,9 @@ class ProductForm extends HTMLElement {
 
   onSubmitHandler(evt) {
     evt.preventDefault();
+    this.itemProperty = document.querySelector('[data-type="pinfel-select"]');
+    this.cartNotification.setActiveElement(document.activeElement);
+
     this.cartNotification.setActiveElement(document.activeElement);
     
     const submitButton = this.querySelector('[type="submit"]');
